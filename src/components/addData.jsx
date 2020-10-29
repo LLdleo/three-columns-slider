a574d19023d75ab3dcb5ef1bc571a376be3d2da1
Lidian Lin <lldlld0574@gmail.com>
2020-10-29 18:16:30 -0400

    small adjustment

--- src/components/addData.jsx
+++ src/components/addData.jsx
@@ -4,7 +4,7 @@ function addData(data) {
   return (
     data.map(d =>
       <div className="threeColumns" key={d.metadata.id}>
-        <div className="metadata"><div className="title">Metadata</div><ReactJson src={d.metadata} /></div>
+        <div className="metadata"><div className="title">Metadata</div><ReactJson src={d.metadata} enableClipboard={false} /></div>
         <div className="img"><div className="title">Image</div><div className='imgRow'><img width="100%" height="100%" src={d.img} alt="" /></div></div>
         <div className="plot"><div className="title">Plot</div>{d.plot}</div>
       </div>
